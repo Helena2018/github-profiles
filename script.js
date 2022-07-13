4af92d2af4abc6db0464b9c527c0e87c03bec860
Helena2018 <helena2020liu@gmail.com>
2022-07-13 20:39:03 +1200

    display later 5 repos

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function getUser(username) {
 
 async function getRepos(username) {
   try {
-    const { data } = await axios(APIURL + username + '/repos')
+    const { data } = await axios(APIURL + username + '/repos?sort=created')
 
     addReposToCard(data)
   } catch(err) {
